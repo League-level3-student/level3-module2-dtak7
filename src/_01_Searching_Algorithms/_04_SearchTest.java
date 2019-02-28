587957ff04157c5cb99006351861a3021dad297a
dtak7 <league@WTS14.attlocal.net>
2019-02-27 18:29:33 -0800

    i am confused by who is the tallest peep

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -22,18 +22,25 @@ class _04_SearchTest {
 		int[] array = { 0, 1, 2, 3, 4, 5 };
 		// 2. use the assertEquals method to test your binary search method.
 		// remember that the array must be sorted
-		assertEquals(2, _01_BinarySearch.binarySearch(array, 0, array.length, 2));
+		assertEquals(2, _01_BinarySearch.binarySearch(array, 0, array.length - 1, 2));
+		assertEquals(-1, _01_BinarySearch.binarySearch(array, 0, array.length - 1, 6));
 	}
 
 	@Test
 	public void testInterpolationSearch() {
+		int[] array = { 0, 1, 2, 3, 4, 5 };
 		// 3. use the assertEquals method to test your interpolation search method.
 		// remember that the array must be sorted and evenly distributed
+		assertEquals(3, _01_BinarySearch.binarySearch(array, 0, array.length - 1, 3));
+		assertEquals(-1, _01_BinarySearch.binarySearch(array, 0, array.length - 1, 6));
 	}
 
 	@Test
 	public void testExponentialSearch() {
+		int[] array = { 0, 1, 2, 3, 4, 5 };
 		// 4. use the assertEquals method to test your exponential search method.
 		// remember that the array must be sorted
+		assertEquals(3, _01_BinarySearch.binarySearch(array, 0, array.length - 1, 3));
+		assertEquals(5, _01_BinarySearch.binarySearch(array, 0, array.length - 1, 5));
 	}
 }
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,32 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static Object countPearls(List<Boolean> oysters) {
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i).equals(true)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		int largest = 0;
+		int contestant = 0;
+		for (int i = 0; i < peeps.size(); i++) {
+			contestant = i;
+			if (contestant > largest) {
+				contestant = largest;
+			}
+		}
+		return largest;
 	}
 }
