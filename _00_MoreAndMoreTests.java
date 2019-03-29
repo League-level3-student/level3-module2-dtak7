2deb20e85349bebfe27b14fc3ac6d03a87b5dd54
dtak7 <hgbaba@gmail.com>
2019-03-29 16:29:12 -0700

    done with module 2
    on to module 3

---
+++ _00_MoreAndMoreTests.java
@@ -0,0 +1,93 @@
+package _02_More_Algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Uncomment each test and then write the method to make it pass.
+ **/
+
+public class _00_MoreAndMoreTests {
+
+	@Test
+	public void MultTest() {
+
+		assertEquals("10 x 0 = 0", multiply(10, 0));
+		assertEquals("10 x 10 = 100", multiply(10, 10));
+		assertEquals("8 x 11 = 88", multiply(8, 11));
+	}
+
+	public Object multiply(int i, int j) {
+		String answer = i + " x " + j + " = " + i * j;
+		return answer;
+	}
+
+	@Test
+	public void PrimeTest() {
+
+		assertTrue(isPrime(3));
+		assertTrue(isPrime(5));
+		assertTrue(isPrime(541));
+		assertFalse(isPrime(4));
+		assertFalse(isPrime(12));
+		assertTrue(isPrime(523));
+
+	}
+
+	public boolean isPrime(int i) {
+		int remainder;
+		for (int j = 2; j <= i / 2; j++) {
+			remainder = i % 2;
+			if (remainder == 0) {
+				return false;
+
+			}
+		}
+		return true;
+	}
+
+	@Test
+	public void SquareTest() {
+
+		assertTrue(isSquare(4));
+		assertTrue(isSquare(144));
+		assertTrue(isSquare(64));
+		assertTrue(isSquare(10201));
+		assertTrue(isSquare(1));
+		assertFalse(isSquare(3));
+		assertFalse(isSquare(22));
+		assertFalse(isSquare(143));
+
+	}
+
+	public boolean isSquare(int i) {
+		int result = (int) Math.sqrt(i);
+		if (result * result == i) {
+			return true;
+		}
+		return false;
+	}
+
+	@Test
+	public void CubeTest() {
+
+		assertTrue(isCube(27));
+		assertTrue(isCube(216));
+		assertTrue(isCube(729));
+		assertTrue(isCube(1));
+		assertFalse(isCube(3));
+		assertFalse(isCube(22));
+		assertFalse(isCube(143));
+
+	}
+
+	public boolean isCube(int i) {
+		int result = (int) Math.cbrt(i);
+		if(Math.pow(result, 3)==i) {
+			return true;
+		}
+		return false;
+	}
+
+}
